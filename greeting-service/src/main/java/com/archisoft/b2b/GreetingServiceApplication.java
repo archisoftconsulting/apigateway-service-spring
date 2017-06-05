0b60a4391d874acd1c8577beb716121607985aa2
Tan <khimsuan.tan@archisoftconsulting.com>
2017-06-05 07:13:25 +0000

    s

--- greeting-service/src/main/java/com/archisoft/b2b/GreetingServiceApplication.java
+++ greeting-service/src/main/java/com/archisoft/b2b/GreetingServiceApplication.java
@@ -2,11 +2,19 @@ package com.archisoft.b2b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GreetingServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GreetingServiceApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String HelloWorld(){
+		return "Hello World";
 	}
 }
