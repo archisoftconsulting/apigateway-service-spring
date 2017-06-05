08e674074236632c9fc0ac2f4e87dfafdf35f097
Tan <khimsuan.tan@archisoftconsulting.com>
2017-06-05 06:38:11 +0000

    initial

---
+++ oauth2-service/src/main/java/com/archisoft/b2b/Auth2ServiceApplication.java
@@ -0,0 +1,12 @@
+package com.archisoft.b2b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth2ServiceApplication.class, args);
+	}
+}
