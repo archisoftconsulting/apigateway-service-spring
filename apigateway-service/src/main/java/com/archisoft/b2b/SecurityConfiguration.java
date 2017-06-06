c9f655584a03a3c7d4ff1e2e8871b56821f5b64b
vagrant <vagrant@vagrant>
2017-06-06 16:53:47 +0800

    test

--- apigateway-service/src/main/java/com/archisoft/b2b/SecurityConfiguration.java
+++ apigateway-service/src/main/java/com/archisoft/b2b/SecurityConfiguration.java
@@ -87,7 +87,7 @@ public class SecurityConfiguration {
 			// .antMatchers(HttpMethod.POST,"/writeonly/**").access("#oauth2.hasScope('b2b')
 			// and hasRole('WRITE')")
 			// .anyRequest().permitAll();
-			//TESTING22222
+			//TESTING222222
 			
 
 			http.authorizeRequests().antMatchers(HttpMethod.GET, "/protected").hasRole("READER")	
