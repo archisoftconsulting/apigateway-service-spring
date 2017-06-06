1cf72e13915b75d4394d24173a0f4987956bb2bb
Tan <khimsuan.tan@archisoftconsulting.com>
2017-06-06 09:09:48 +0000

    dd

--- apigateway-service/src/main/java/com/archisoft/b2b/SecurityConfiguration.java
+++ apigateway-service/src/main/java/com/archisoft/b2b/SecurityConfiguration.java
@@ -33,28 +33,6 @@ public class SecurityConfiguration {
 			// and hasRole('WRITE')")
 			// .anyRequest().permitAll();
 			
-<<<<<<< HEAD
-<<<<<<< HEAD
-			 @Override
-			 public void configure(final HttpSecurity http) throws Exception {
-			        // @formatter:off
-//			  http.authorizeRequests()
-//			  	.antMatchers(HttpMethod.GET,"/readonly/**").access("#oauth2.hasScope('b2b') and hasRole('READ')")
-//			  	
-//			  	.antMatchers(HttpMethod.POST,"/writeonly/**").access("#oauth2.hasScope('b2b') and hasRole('WRITE')")
-//			    .anyRequest().permitAll();
-			  
-			  http.authorizeRequests()
-			  	.antMatchers(HttpMethod.POST, "/protected").hasRole("WRITER")
-			  	.antMatchers(HttpMethod.GET, "/protected").hasRole("READER")
-			  	//.anyRequest().authenticated();
-			  	.anyRequest().permitAll();
-			  
-			  
-			 }
-			
-=======
-=======
 
 			http.authorizeRequests().antMatchers(HttpMethod.GET, "/protected").hasRole("READER")	
 			.antMatchers(HttpMethod.POST, "/protected").hasRole("WRITER")
@@ -62,19 +40,7 @@ public class SecurityConfiguration {
 					.permitAll();
 
 		}
->>>>>>> branch 'master' of https://github.com/archisoftconsulting/apigateway-service-spring
 
-<<<<<<< HEAD
-			http.authorizeRequests().antMatchers(HttpMethod.GET, "/protected").hasRole("READER")	
-			.antMatchers(HttpMethod.POST, "/protected").hasRole("WRITER")
-			.anyRequest()
-					.permitAll();
-
->>>>>>> branch 'master' of https://github.com/archisoftconsulting/apigateway-service-spring
-		}
-
-=======
->>>>>>> branch 'master' of https://github.com/archisoftconsulting/apigateway-service-spring
 	}
 
 //	@Configuration
