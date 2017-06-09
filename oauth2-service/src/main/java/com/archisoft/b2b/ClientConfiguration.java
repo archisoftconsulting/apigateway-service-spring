0553d9a4b7f6d7e7be6b15bfb61205433619d633
Tan <khimsuan.tan@archisoftconsulting.com>
2017-06-09 14:34:24 +0000

    cors origin

--- oauth2-service/src/main/java/com/archisoft/b2b/ClientConfiguration.java
+++ oauth2-service/src/main/java/com/archisoft/b2b/ClientConfiguration.java
@@ -23,8 +23,11 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public class ClientConfiguration{
+	
 	@Configuration
 	@EnableAuthorizationServer
 	protected static class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
--- oauth2-service/src/main/java/com/archisoft/b2b/OAuth2ServiceApplication.java
+++ oauth2-service/src/main/java/com/archisoft/b2b/OAuth2ServiceApplication.java
@@ -2,7 +2,8 @@ package com.archisoft.b2b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
+@CrossOrigin
 @SpringBootApplication
 public class OAuth2ServiceApplication {
 
